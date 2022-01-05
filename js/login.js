2616141f1744ad74d3f2e88942556593128db877
AM-student <93679555+AM-student@users.noreply.github.com>
2022-01-05 23:08:07 +0100

    some fixes

--- js/login.js
+++ js/login.js
@@ -4,21 +4,23 @@ function validate(){
     var password = document.getElementById("password").value;
     if (username == "admin" && password == "admin"){
         alert ("Login successfully");
-        window.location = "../aboutus/aboutus.html";
+        window.location = "#";
         return false;
     }
-    else if(username != null && password != null){
+    else if(username == null && password == null){
         alert ("Enter your username and password");
+        return false;
     }
     else{
         attempt --;
-        alert("You have left "+attempt+" attempt;");
-    
+        alert("Your username and password was incorrect, you have "+ attempt+ " attempts left");
         if( attempt == 0){
             document.getElementById("username").disabled = true;
             document.getElementById("password").disabled = true;
             document.getElementById("submit").disabled = true;
         return false;
-        }
+        }    
     }
+    /**/
+    return false;
 }
