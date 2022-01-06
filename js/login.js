960ac30960ea1ee3f8b2a74b86ba412234caec53
AM-student <93679555+AM-student@users.noreply.github.com>
2022-01-06 18:12:41 +0100

    some changes in js

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ function validate(){
     var password = document.getElementById("password").value;
     if (username == "admin" && password == "admin"){
         alert ("Login successfully");
-        window.location = "#";
+        window.location = "../Home.html";
         return false;
     }
     else if(username == null && password == null){
