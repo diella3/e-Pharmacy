96f5d9c2a9a14e18614a9ccdfac1598841f579aa
Plarentine <65871473+Plarentine@users.noreply.github.com>
2022-02-17 18:26:03 +0100

    admin

---
+++ admin.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        
+        <title>Administrator dashboard</title>
+
+        <link rel="stylesheet" type="text/css" href="./css/admin.css">
+
+    </head>
+
+
+<body>
+    <header class="header">
+
+        <div class=left-area>
+            <img src="./assets/pharmacyLogo.png" alt="Logo photo is added here">
+            <h1>e-Pharmacy</h1>
+        </div>
+    
+        <div class="right-area">
+            <a href="#" class=" logout_btn">Logout</a>
+        </div>
+    </header>
+
+
+    <div class="sidebar">
+        <div class="image">
+            <img src="./assets/adm.jpg" alt="adminicon is added here">
+        </div>
+
+        <div class="sidebarNavButtons">
+
+            <div class="navBtn"><a href="perdorues.html" class="link">Users</a></div>        
+            <div class="navBtn"><a href="#" class="link">Guests</a></div>             
+            <div class="navBtn"><a href="#" class="link">Test</a></div>        
+            <div class="navBtn"><a href="#" class="link">Test</a></div>        
+        </div>
+
+    </div>
+
+</body>
+
+</html>
