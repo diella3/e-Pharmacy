b01719afbad4a14c8bdf6abe11b9916eb303466d
AM-student <93679555+AM-student@users.noreply.github.com>
2022-03-01 18:00:25 +0100

    fix

--- admin/admRegister/userfunctions.php
+++ admin/admRegister/userfunctions.php
@@ -21,39 +21,4 @@ class userfunctions{
 
         return $users;
     }
-
-    function getUserById($userid)
-    {
-        $conn = $this->db->pdo;
-
-        $sql = "SELECT userid,username,password,email,firstname,lastname,address,zip FROM users WHERE id=?";
-        $statement=$conn->prepare($sql);
-        $user = $statement->execute([$userid]);
-
-        return $user->fetch(PDO::FETCH_ASSOC);
-    }
-
-
-    function updateUser($username, $password, $email, $firstname, $lastname, $address, $zip)
-    {
-        $conn = $this->db->pdo;
-
-        $sql = "UPDATE userS SET password=?,email=?,firstname=?,lastname=?,address=?,zip=?,password=? where userId=?";
-
-        $statement = $conn->prepare($sql);
-
-        $statement->execute([$username, $password, $email, $firstname, $lastname, $address, $zip]);
-        echo "<script> alert('User has been updated successfully!') </script>";
-    }
-
-    function deleteUserById($userId)
-    {
-        $conn = $this->db->pdo;
-
-        $sql = "DELETE FROM userS WHERE id=?";
-
-        $statement = $conn->prepare($sql);
-        $statement->execute([$userId]);
-        echo "<script> alert('User has been deleted successfully!') </script>";
-    }
 }
